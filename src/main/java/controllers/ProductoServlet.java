a6b5705979439b9fddbbad5e219c483a6ae548a5
DiegoB <diegobaesv@gmail.com>
2025-03-15 19:37:43 -0500

    productos completos

--- src/main/java/controllers/ProductoServlet.java
+++ src/main/java/controllers/ProductoServlet.java
@@ -1,39 +1,40 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Producto;
+import models.Subcategoria;
 import services.CategoriaService;
+import services.ProductoService;
 import services.impl.CategoriaServiceImpl;
+import services.impl.ProductoServiceImpl;
 
 @WebServlet("/productos")
 public class ProductoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private final ProductoService productoService;
        
     public ProductoServlet() {
         super();
+        productoService = new ProductoServiceImpl();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		response.getWriter().append("METODO GET - Served at: ").append(request.getContextPath());
+		Integer idSubcategoria = Integer.parseInt(request.getParameter("idSubcategoria"));
+		List<Producto> productos = productoService.listarProductosPorIdSubcategoria(idSubcategoria);
+		request.setAttribute("productos", productos);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/views/productos.jsp");
+		dispatcher.forward(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("METODO POST - Served at: ").append(request.getContextPath());
-	}
-	
-	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		response.getWriter().append("METODO PUT - Served at: ").append(request.getContextPath());
-	}
-	
-	protected void doPatch(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("METODO PATCH - Served at: ").append(request.getContextPath());
-	}
 
 }
--- src/main/webapp/views/productos.jsp
+++ src/main/webapp/views/productos.jsp
@@ -1,7 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ page import="java.util.List" %>
-<%@ page import="models.Subcategoria" %>
+<%@ page import="models.Producto" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,7 +13,7 @@
 <body>
 
 	<%
-		List<Subcategoria> subcategorias = (List<Subcategoria>) request.getAttribute("subcategorias");
+		List<Producto> productos = (List<Producto>) request.getAttribute("productos");
 	%>
 
 	<header>
@@ -26,11 +26,12 @@
 	
 	<main class="p-4">
 		<div class="flex w-full justify-center p-4 gap-4 flex-wrap">
-			<% if (subcategorias != null && !subcategorias.isEmpty()) { %>
-				<% for(Subcategoria subcategoria: subcategorias) { %>
-					<a href="/galactus-store/productos?idSubcategoria=<%= subcategoria.getIdSubcategoria() %>" class="flex bg-gray-200 w-[200px] justify-center flex-col p-4 gap-2 items-center rounded-md border border-gray-300 border-solid hover:border-green-600 cursor-pointer">
-						<img class="rounded-sm" src="<%= subcategoria.getImagenUrl() %>" width="150px">
-						<span class="text-center font-bold text-gray-600"><%= subcategoria.getNombre() %></span>
+			<% if (productos != null && !productos.isEmpty()) { %>
+				<% for(Producto producto: productos) { %>
+					<a class="flex bg-gray-200 w-[200px] justify-center flex-col p-4 gap-2 items-center rounded-md border border-gray-300 border-solid hover:border-green-600 cursor-pointer">
+						<!-- img class="rounded-sm" src="" width="150px"!-->
+						<span class="text-center text-green-600"><%= producto.getCodigoInterno() %></span>
+						<span class="text-center font-bold text-gray-600"><%= producto.getNombre() %></span>
 					</a>
 				<% 	} %>
 			<%	} else { %>
@@ -38,8 +39,8 @@
 			<% } %>
 		</div>
 		
-		<% if (subcategorias != null && !subcategorias.isEmpty()) { %>
-			<div class="w-full p-4">Total registros: <%= subcategorias.size() %></div>
+		<% if (productos != null && !productos.isEmpty()) { %>
+				<div class="w-full p-4">Total registros: <%= productos.size() %></div>
 		<% } %>
 		
 		
